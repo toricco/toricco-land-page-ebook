b841ae539f2304269cae4b3d9770a0cc15846532
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-04-14 11:21:22 -0300

    refactor: update image book-and-cell

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,7 +3,7 @@ export { default as HeaderBackground } from './header-background.svg';
 export { default as Check } from './check.svg';
 export { default as Circle } from './circle.svg';
 export { default as ToRicco } from './toricco.png';
-export { default as ebookAndCell } from './ebook-Mockup-with-iphone.png';
+export { default as ebookAndCell } from './book-and-iphone.png';
 export { default as DigitalEbook } from './Digital-Mockup.png';
 export { default as LogoFooter } from './LogoFooter.png';
 export { default as IconFace } from './IconFace.png';
